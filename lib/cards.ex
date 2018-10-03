7c7183e8d0c0a57781939322542065f9aeaf0dcc
Maxime Heroux <m.heroux@mondiako.com>
2018-10-03 18:16:45 -0400

    First Commit

---
+++ lib/cards.ex
@@ -0,0 +1,34 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+
+  """
+  def nom do
+    "maddox heroux"
+  end
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for value <- values, suit <- suits do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  # { hand: [5], deck: [rest]}
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+end
