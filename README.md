944a309abd675d3ee61378bc90f4ff383183806b
MaxHer0 <43393259+MaxHer0@users.noreply.github.com>
2018-10-07 16:05:13 -0400

    backup

--- README.md
+++ README.md
@@ -19,3 +19,13 @@ Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_do
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at [https://hexdocs.pm/cards](https://hexdocs.pm/cards).
 
+  """
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :error -> "msg error"
+      :ok -> :erlang.binary_to_term(binary)
+    end
+  end
+  """
--- lib/cards.ex
+++ lib/cards.ex
@@ -4,12 +4,8 @@ defmodule Cards do
   """
 
   @doc """
-
+    Return a list of strings representing a deck of playing cards
   """
-  def nom do
-    "maddox heroux"
-  end
-
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -23,12 +19,53 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determine whether a deck contains a given cards
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
   # { hand: [5], deck: [rest]}
+  # Pattern matching = variable assignment
+
+  @doc """
+  Divides a desk into a hand the remainder of the deck.
+  The `hand_size` argument indicates how many cards should
+  be in the deck
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:error, _reason} -> "msg error"
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck() |> Cards.shuffle() |> Cards.deal(hand_size)
   end
 end
